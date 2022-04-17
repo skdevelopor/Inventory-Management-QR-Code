0f968dbf38ade38fa839a9d15896de8a6b412f28
siddharth <siddharthbraindelta@gmail.com>
2022-04-17 21:04:17 +0530

    added View ALL with progress Bars

--- app/src/main/java/sk/dev/qr_ims/viewAll.java
+++ app/src/main/java/sk/dev/qr_ims/viewAll.java
@@ -9,18 +9,19 @@ import com.firebase.ui.database.FirebaseRecyclerOptions;
 import com.google.firebase.database.FirebaseDatabase;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.ProgressBar;
 
 public class viewAll extends AppCompatActivity {
 RecyclerView recyclerView;
 AdapterClass myAdapter;
-
+ProgressBar pbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_all);
-
+      pbar = findViewById(R.id.progressBar4);
         recyclerView = findViewById(R.id.recView);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
@@ -32,6 +33,41 @@ AdapterClass myAdapter;
 
         myAdapter = new AdapterClass(options);
        recyclerView.setAdapter(myAdapter);
+
+
+
+
+
+
+        Handler handler = new Handler();
+
+// Create and start a new Thread
+        new Thread(new Runnable() {
+            public void run() {
+                try{
+                    Thread.sleep(2000);
+                }
+                catch (Exception e) { } // Just catch the InterruptedException
+
+                // Now we use the Handler to post back to the main thread
+                handler.post(new Runnable() {
+                    public void run() {
+                        // Set the View's visibility back on the main UI Thread
+                        pbar.setVisibility(View.INVISIBLE);
+                    }
+                });
+            }
+        }).start();
+
+
+
+
+
+
+
+
+
+
 
 
 
